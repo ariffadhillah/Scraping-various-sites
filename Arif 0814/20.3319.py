5417eab3f69c37741152bb53a59109b1bff108c0
ariffadhillah <ariffadhillah319@gmail.com>
2023-08-18 14:48:05 +0700

    success filename 20.3319 task 0814

--- Arif 0814/20.3319.py
+++ Arif 0814/20.3319.py
@@ -9,7 +9,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203'
 }
 
-fields = ['Names', 'Titles' , 'Biography']
+fields = ['Names', 'Roles' , 'Biography']
 filename = '20.3319.csv'
 
 data = []
@@ -21,15 +21,43 @@ r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.content, 'lxml')
 
 find_section = soup.find('div', class_='flex-row')
-print(find_section)
+# print(find_section)
 
 
-# for linkItem in find_section.find_all('a',  href=True):
-#     urlbio = linkItem['href'].replace('/about-nlrb/who-we-are/board/','')
-#     # print(urlbio)
-#     urldetail = 'https://www.nlrb.gov/bio/' + urlbio
+for linkItem in find_section.find_all('a',  href=True):
+    urlbio = linkItem['href'].replace('/about-nlrb/who-we-are/board/','').replace('/bio/david-m-prouty','david-m-prouty')
+    # print(urlbio)
+    urldetail = 'https://www.nlrb.gov/bio/' + urlbio
 
-#     if urldetail not in processed_urls:
-#         list_item.append((urldetail))  
-#         processed_urls.add(urldetail)
-# print(list_item)
+    if urldetail not in processed_urls:
+        list_item.append((urldetail))  
+        processed_urls.add(urldetail)
+
+for bioDetail in list_item:  
+    r = requests.get(bioDetail, headers=headers)
+    soup = BeautifulSoup(r.content, 'lxml')
+    
+    print(bioDetail)
+
+    name = soup.find('h1', class_='uswds-page-title page-title').text.strip()
+    roles = soup.find('div', class_='field field--name-field-bio__officer-title field--type-string field--label-hidden field__item').text.strip()
+
+    section_bio = soup.find('div', class_='node__content')
+
+    if section_bio:
+        biography_paragraphs = section_bio.find_all('p')
+        biography = '\n\n'.join([p.text for p in biography_paragraphs]) 
+
+    data_save = {
+        'Names' : name,
+        'Roles' : roles,
+        'Biography' : biography
+    }
+
+    data.append(data_save)
+    print('Saving', data_save['Names'])
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(data)
+
--- Arif 0814/20.3320.py
+++ Arif 0814/20.3320.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 import csv
 import re
 
-url = 'https://new.nsf.gov/about/leadership'
+url = 'https://new.nsf.gov/about/leadership#od'
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203'
 }
 
 fields = ['Names', 'Titles' , 'Biography']
-filename = '20.3319.csv'
+filename = '20.3320.csv'
 
 data = []
 
@@ -22,12 +22,26 @@ soup = BeautifulSoup(r.content, 'lxml')
 
 find_section = soup.find_all('div', class_='palette palette--light-gray layout-container--wrapper nsf-layout nsf-layout--threecol full-browser-width')
 
-for info in find_section:
-    titleelement = info.find_all('div', class_='text__content text-formatted')
+for card_ in find_section:
+    card_item = card_.find('div', class_='nsf-layout--outer')
     
-    for title_elem in titleelement:
-        h4_elements = title_elem.find_all('h2')
-        for titleh4 in h4_elements:
-            title = titleh4.text.strip()
-            print(title)
+    for title_element in card_item:
+        titleelement = title_element.find('div', class_='text__content text-formatted')
+        # print(titleelement)
+        for title_elem in titleelement:
+            h4_elements = title_elem.find('h2')
+            for titleh4 in h4_elements:
+                title = titleh4.text.strip()
+                print(title)
 
+# for info in find_section:
+
+# for infocardName in find_section:
+        
+#     cardinfo = infocardName.find_all('div', {'class':['nsf-layout--inner']})
+#     nsf-layout--outer
+# text__content text-formatted
+
+# for find_card_item in find_section:
+#     card_item = find_card_item.find('div', class_='nsf-layout--outer')
+#     print(card_item)
